f99270239f0ea9acff17ad7dcf0e62565eaf8a88
azizios011 <hdriaziz699@gmail.com>
2023-11-21 14:17:15 +0100

    Hello Flask!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
+""" 
+    a Flask web application
 
+"""
 from flask import Flask
 
 app = Flask(__name__)
 @app.route("/")
+"""
+a route to display the text
+
+"""
 def hello_hbnb():
     return 'Hello HBNB'
 
